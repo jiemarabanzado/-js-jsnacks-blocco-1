23c230c4215662a717afbcccd73f268046b6dbdb
jiemarabanzado <jiemar.abanzado@yahoo.it>
2022-10-26 12:00:19 +0200

    consegna snakc 4

---
+++ jsnack4/main.js
@@ -0,0 +1,9 @@
+const invitati=["marco","sergio,","franco"];
+let risposta="non sei invitato";
+let nome=prompt('inserisci il tuo nome');
+for(let index=0; index<invitati.length;index++){
+    if(nome===invitati[index]){
+        risposta='sei invitato';
+    }
+}
+console.log(nome,risposta);
