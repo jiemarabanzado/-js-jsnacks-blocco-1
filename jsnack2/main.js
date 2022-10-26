3f45f5ee64a5d18cc33af40119d4a8914601fac3
jiemarabanzado <jiemar.abanzado@yahoo.it>
2022-10-26 10:52:36 +0200

    consegna

---
+++ jsnack2/main.js
@@ -0,0 +1,9 @@
+let a=prompt('scrivi una parola');
+let b=prompt('scrivi una parola');
+if(a.length<b.length){
+    console.log("piu corta:",a);
+    console.log(b);
+}else{
+    console.log("piu corta:",b);
+    console.log(a);
+}
