9f52f7bc9d7fb3fa8535fc4301c50d8be1e94fd5
jiemarabanzado <jiemar.abanzado@yahoo.it>
2022-10-26 10:57:57 +0200

    uguaglianza

--- jsnack2/main.js
+++ jsnack2/main.js
@@ -1,9 +1,11 @@
 let a=prompt('scrivi una parola');
 let b=prompt('scrivi una parola');
-if(a.length<b.length){
+if(a.length==b.length){
+    console.log('parole lunghe uguali');
+}else if(a.length<b.length){
     console.log("piu corta:",a);
     console.log(b);
-}else{
+    }else{
     console.log("piu corta:",b);
     console.log(a);
-}
+    }
